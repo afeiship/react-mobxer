60271940cebd0b49d5bd70b456f33384d5041ede
afeiship <1290657123@qq.com>
2017-06-16 22:14:02 +0800

    intail & finish

---
+++ dist/next-local-storage.js
@@ -0,0 +1,25 @@
+(function () {
+
+  'use strict';
+  
+  var global = global || window || this;
+  var nx = global.nx || require('next-js-core2');
+  var NxAbstractStorage = nx.AbstractStorage || require('next-abstract-storage');
+
+  var NxLocalStorage = nx.declare('nx.LocalStorage', {
+    extends: NxAbstractStorage,
+    methods:{
+      init: function(inPrefix){
+        this.base(this,{
+          engine:'localStorage',
+          prefix: inPrefix || ''
+        });
+      }
+    }
+  });
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxLocalStorage;
+  }
+
+}());
