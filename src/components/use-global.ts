2ed54ddf447107ab7b70e6361e61491ad3b11c7a
afeiship <1290657123@qq.com>
2022-08-16 16:04:29 +0800

    fix: rm ctx impl

--- src/components/use-global.ts
+++ src/components/use-global.ts
@@ -1,7 +1,5 @@
-import { createContext, useContext, useRef } from 'react';
 import { sharedStore } from './config-provider';
 
 export default () => {
-  const storeCtx = useRef(createContext(sharedStore.compositeStore));
-  return useContext(storeCtx.current);
+  return sharedStore.compositeStore;
 };
