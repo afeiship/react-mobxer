ac792caa2389654e1e968001128ad06bf36e17fe
afeiship <1290657123@qq.com>
2023-04-17 17:40:16 +0800

    feat: add autorun opts

--- src/components/watch.ts
+++ src/components/watch.ts
@@ -1,15 +1,16 @@
 import { toJS, autorun } from 'mobx';
 import { useEffect } from 'react';
+import type { IAutorunOptions } from 'mobx';
 
 type WatchCallback = (inPlain: any) => void;
 
-export function watch(inTarget: any, inCallback: WatchCallback) {
+export function watch(inTarget: any, inCallback: WatchCallback, inOptions?: IAutorunOptions) {
   return autorun(() => {
     const plain = toJS(inTarget);
     inCallback(plain);
-  });
+  }, inOptions);
 }
 
-export const useWatch = (inTarget: any, inCallback: WatchCallback) => {
-  useEffect(() => watch(inTarget, inCallback), []);
+export const useWatch = (inTarget: any, inCallback: WatchCallback, inOptions?: IAutorunOptions) => {
+  useEffect(() => watch(inTarget, inCallback, inOptions), []);
 };
